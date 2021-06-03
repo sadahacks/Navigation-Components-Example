cf4fe799b4fcda515e1630dc06ce99706732902f
Sadaqat Hussain <70800337+sadahacks@users.noreply.github.com>
2021-06-03 21:19:25 +0500

    Add files via upload

---
+++ main/java/com/example/navigation_components_example/VerificationFragment.java
@@ -0,0 +1,87 @@
+package com.example.navigation_components_example;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavOptions;
+import androidx.navigation.Navigation;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link VerificationFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class VerificationFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public VerificationFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment VerificationFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static VerificationFragment newInstance(String param1, String param2) {
+        VerificationFragment fragment = new VerificationFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_verification, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        NavController navController= Navigation.findNavController(view);
+        Button button=view.findViewById(R.id.varify_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //navController.navigate(R.id.action_verificationFragment_to_loginFragment3);
+
+                NavOptions navOptions= new NavOptions.Builder().setPopUpTo(R.id.loginFragment3,true).build();
+
+                navController.navigate(R.id.action_verificationFragment_to_loginFragment3,null,navOptions);
+            }
+        });
+    }
+}
